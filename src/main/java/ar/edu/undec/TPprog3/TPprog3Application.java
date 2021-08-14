6424a2ac8606b292afc64ffd6cb9696d0b0a54c9
Max Reyna <maxla070212@gmail.com>
2021-08-14 19:55:57 -0300

    commit inicial
    Comenzamos.

---
+++ src/main/java/ar/edu/undec/TPprog3/TPprog3Application.java
@@ -0,0 +1,13 @@
+package ar.edu.undec.TPprog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TPprog3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TPprog3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/undec/TPprog3/TPprog3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.undec.TPprog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TPprog3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
